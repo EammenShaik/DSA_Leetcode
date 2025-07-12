de54f82a48caeb47ece0fbd1be64b2cdf0f6777d
Eammen Shaik <24mc3044@rgipt.ac.in>
2025-07-12 10:31:13 +0530

    Add files via upload

---
+++ 160_Intersection_of_Two_Linked_Lists/160_Intersection_of_Two_Linked_List.c
@@ -0,0 +1,12 @@
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (!headA || !headB) return NULL;
+
+    struct ListNode *pA = headA;
+    struct ListNode *pB = headB;
+
+    while (pA != pB) {
+        pA = (pA) ? pA->next : headB;
+        pB = (pB) ? pB->next : headA;
+    }
+    return pA;
+}
